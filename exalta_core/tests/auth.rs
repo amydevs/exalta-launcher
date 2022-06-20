331f26f52208eed03b2ff53fe0a77c7e86a2b258
amy <ctrlalternativedelete@gmail.com>
2022-06-20 13:40:31 +1000

    updated auth tests

--- exalta_core/tests/auth.rs
+++ exalta_core/tests/auth.rs
@@ -1,22 +1,23 @@
 #[cfg(test)]
 mod tests {
+    use exalta_core::auth::{account::Account, AuthInfo};
+
     #[tokio::test]
     async fn test_login() {
-        let (u, p) = get_env_up();
-        let client = exalta_core::auth::AuthInfo::default().username_password(&u, &p);
-        exalta_core::auth::request_account(&client).await.unwrap();
+        exalta_core::auth::request_account(&get_env_auth_info()).await.unwrap();
     }
 
     #[tokio::test]
     async fn test_verify() {
-        let (u, p) = get_env_up();
-        let client = exalta_core::auth::AuthInfo::default().username_password(&u, &p);
-        let account = exalta_core::auth::request_account(&client).await.unwrap();
+        let account = exalta_core::auth::request_account(&get_env_auth_info()).await.unwrap();
         exalta_core::auth::verify_access_token(&account.access_token).await.unwrap();
     }
 
-    fn get_env_up() -> (String, String) {
+    fn get_env_auth_info() -> AuthInfo {
         dotenv::from_path("./tests/.env").unwrap();
-        (std::env::var("USERNAME").unwrap(), std::env::var("PASSWORD").unwrap())
+        exalta_core::auth::AuthInfo::default().username_password(
+            &std::env::var("USERNAME").unwrap(),
+            &std::env::var("PASSWORD").unwrap()
+        )
     }
 }

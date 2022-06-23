ccba3771c9e6bf81031b2d9c3e13cd471d0530f9
Amy Y <ctrlalternativedelete@gmail.com>
2022-06-23 23:42:53 +1000

    fmt

--- exalta_core/src/download/mod.rs
+++ exalta_core/src/download/mod.rs
@@ -1,5 +1,8 @@
 use once_cell::sync::Lazy;
-use reqwest::{Url, Method, header::{HeaderMap, HeaderValue}};
+use reqwest::{
+    header::{HeaderMap, HeaderValue},
+    Method, Url,
+};
 
 use crate::CLIENT;
 
@@ -7,9 +10,13 @@ use self::checksumfiles::ChecksumFiles;
 
 mod checksumfiles;
 
-static BUILD_URL: Lazy<Url> = Lazy::new(|| Url::parse("https://rotmg-build.decagames.com/").unwrap());
+static BUILD_URL: Lazy<Url> =
+    Lazy::new(|| Url::parse("https://rotmg-build.decagames.com/").unwrap());
 
-pub async fn request_checksums(build_hash: &str, platform: &str) -> Result<ChecksumFiles, Box<dyn std::error::Error>> {
+pub async fn request_checksums(
+    build_hash: &str,
+    platform: &str,
+) -> Result<ChecksumFiles, Box<dyn std::error::Error>> {
     let url = get_build_url(build_hash, platform, "checksum.json")?;
 
     // let mut defheaders = DEFAULT_HEADERS.clone();
@@ -19,14 +26,20 @@ pub async fn request_checksums(build_hash: &str, platform: &str) -> Result<Check
     let mut defheaders = HeaderMap::new();
     defheaders.append("Host", BUILD_URL.host_str().unwrap().parse()?);
 
-    let resp = CLIENT.request(Method::GET, url).headers(defheaders).send().await?;
+    let resp = CLIENT
+        .request(Method::GET, url)
+        .headers(defheaders)
+        .send()
+        .await?;
     let resp_text = resp.text().await?;
-    println!("{}", resp_text);
-    
 
     Ok(serde_json::from_str::<ChecksumFiles>(&resp_text)?)
 }
 
-pub fn get_build_url(build_hash: &str, platform: &str, file: &str) -> Result<Url, Box<dyn std::error::Error>> {
+pub fn get_build_url(
+    build_hash: &str,
+    platform: &str,
+    file: &str,
+) -> Result<Url, Box<dyn std::error::Error>> {
     Ok(BUILD_URL.join(format!("build-release/{}/{}/{}", build_hash, platform, file).as_str())?)
 }

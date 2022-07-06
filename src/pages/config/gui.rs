c9f371f2291536d2c0f967b5107cb643b184a499
amy <ctrlalternativedelete@gmail.com>
2022-07-07 04:52:57 +1000

    fixed gui for config

--- src/pages/config/gui.rs
+++ src/pages/config/gui.rs
@@ -5,14 +5,13 @@ use crate::ExaltaLauncher;
 impl ExaltaLauncher {
     pub fn render_config(&mut self, ui: &mut Ui) -> Result<(), Box<dyn std::error::Error>> {
         let mut changed = false;
-        ui.vertical_centered_justified(|ui| {
-            let dark_clicked = ui
-                .horizontal(|ui| {
-                    ui.label("Dark Mode:");
-                    ui.checkbox(&mut self.config.dark, "")
-                })
-                .inner
-                .changed();
+        egui::Grid::new("options_grid")
+        .num_columns(2)
+        .striped(true)
+        .show(ui, |ui| {
+
+            ui.label("Dark Mode:");
+            let dark_clicked = ui.checkbox(&mut self.config.dark, "").changed();
             if dark_clicked {
                 changed = dark_clicked;
                 if self.config.dark {
@@ -21,6 +20,13 @@ impl ExaltaLauncher {
                     ui.ctx().set_visuals(egui::Visuals::light());
                 }
             }
+            ui.end_row();
+
+            ui.label("Game Install Path:");
+            if ui.add(egui::TextEdit::singleline(&mut self.config.game_folder_path).hint_text("Write something here")).changed() {
+                changed = true;
+            }
+            ui.end_row();
 
             if changed {
                 self.config.save()?;

3487861ff65f2b7cce728f2cb32da81f5e844f52
amy <ctrlalternativedelete@gmail.com>
2022-07-22 23:37:23 +1000

    fixed trywrite

--- exalta_core/src/lib.rs
+++ exalta_core/src/lib.rs
@@ -56,7 +56,7 @@ fn get_base_url_from_build_type(build_type: &Build) -> &'static Url {
 }
 
 pub fn set_build_force(build: Build) {
-    *BUILD_TYPE.write().unwrap() = build;
+    *BUILD_TYPE.try_write().unwrap() = build;
 }
 
 pub fn set_steamid_game_net_play_platform(steamid: &str) {

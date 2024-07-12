a767baef56e128fef30b4f8666361f27a9b86998
Amy Yan <ayanamydev@gmail.com>
2024-07-12 14:14:55 +1000

    feat: added nix shell

---
+++ shell.nix
@@ -0,0 +1,29 @@
+{ pkgs ? import ./pkgs.nix {}, ci ? false }:
+
+with pkgs;
+mkShell {
+  nativeBuildInputs = [
+    gitAndTools.gh
+    # Rust
+    rustc
+    cargo
+    gcc
+    rustfmt
+    clippy
+    # Deps
+    pkg-config
+    openssl
+    gtk3
+  ];
+  # Don't set rpath for native addons
+  NIX_DONT_SET_RPATH = true;
+  NIX_NO_SELF_RPATH = true;
+  RUST_SRC_PATH = "${rustPlatform.rustLibSrc}";
+  PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+  LD_LIBRARY_PATH = lib.makeLibraryPath [
+    openssl
+  ];
+  shellHook = ''
+
+  '';
+}
